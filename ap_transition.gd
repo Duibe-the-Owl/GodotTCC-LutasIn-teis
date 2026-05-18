2f24b5dce8aaa7e84d070ca2a2d1612abb34d4ca
Bernardo Mafra <be.mafra03@gmail.com>
2026-05-18 18:27:54 -0300

    Seta (Updates)

--- ap_transition.gd
+++ ap_transition.gd
@@ -5,7 +5,18 @@ extends Area3D
 
 @onready var sound_player = $InteractionSound # Reference to the child node
 
+var has_triggered = false
+
+func _ready():
+	# Connect the body entered signal to our function below
+	body_entered.connect(_on_body_entered)
+
 func _on_body_entered(body):
-	if body.is_in_group("player"):
-		# Pass both the path AND the sound resource
+	if (body.name == "Player" or body.is_in_group("Player")) and not has_triggered:
+		has_triggered = true
 		SceneManager.transition_to(target_scene_path, interaction_sound)
+		var active_arrow = get_tree().get_first_node_in_group("ObjectiveArrows")
+		if active_arrow:
+			active_arrow.complete_objective()
+
+		queue_free()
